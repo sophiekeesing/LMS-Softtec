f5c2e441c89a0444b1c2f90ad467bb0184360d0f
Sophie Kaitlin Keesing <sophie.keesing@pino-gmbh.de>
2025-07-10 12:39:08 +0200

    feat(frontend): created header and nav

--- LMS-Softtec/src/App.vue
+++ LMS-Softtec/src/App.vue
@@ -1,45 +1,45 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.png" width="125" height="125" />
-    <div>
-      <h1>Welcome to LMS-Softtec!</h1>
-    </div>
+  <header class="inline-flex h-16 bg-green-800 w-full">
+    <img
+      alt="LMS-Softtec logo"
+      class="w-[50px] h-[50px] m-1.5"
+      src="./assets/logo.png"
+    />
+    <nav class="items-center ml-8 inline-flex">
+      <ul class="inline-flex space-x-5">
+        <li class="underline text-white">
+          <a href="#/">Home</a>
+        </li>
+        <li class="underline text-white">
+          <a href="#/calculator">Calculator</a>
+        </li>
+      </ul>
+    </nav>
   </header>
 
   <main>
-    
+    <component :is="currentView" />
   </main>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from "vue";
+import Calculator from "./components/Calculator.vue";
+import Home from "./components/Home.vue";
+import type { Component } from "vue";
 
+const routes: Record<string, Component> = {
+  "#/": Home,
+  "#/calculator": Calculator,
+};
+
+const currentPath = ref(window.location.hash || "#/");
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash || "#/";
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value] || Home;
+});
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ LMS-Softtec/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center mt-12 text-black text-xl">
+        <h1>
+            Welcome to LMS-Softtec
+        </h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
