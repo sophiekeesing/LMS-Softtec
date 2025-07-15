9564b835f8a1d0c4b4e507a830a1cb679cfba28c
Matix-Media <heilmann@matix-media.net>
2025-07-15 10:52:34 +0200

    fix: :sparkles: change map style to cooler map

--- LMS-Softtec/src/components/MapContainer.vue
+++ LMS-Softtec/src/components/MapContainer.vue
@@ -181,9 +181,11 @@ function initMap() {
         console.log("Map initialized at:", HAMBURG_CENTER);
 
         // Add tile layer
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: "© OpenStreetMap contributors",
-            maxZoom: 19,
+        L.tileLayer("https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png", {
+            minZoom: 0,
+            maxZoom: 18,
+            attribution:
+                '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
 
         console.log("Tile layer added");
