cbb7eef4926f9b9994084618ed9c6d029f1f3a3b
Matix-Media <heilmann@matix-media.net>
2025-07-17 10:36:43 +0200

    feat: :lipstick: add editorconfig file for code style guide

--- LMS-Softtec/src/App.vue
+++ LMS-Softtec/src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div id="app">
-    <div class="bg-[#ACCACE] absolute top-0 left-0 w-full z-10">
-      <img
-        src="./assets/img/ScooTec3.png"
-        class="max-w-[10rem] flex shrink-1 p-1"
-      />
+    <div id="app">
+        <div class="bg-[#ACCACE] absolute top-0 left-0 w-full z-10">
+            <img
+                src="./assets/img/ScooTec3.png"
+                class="max-w-[10rem] flex shrink-1 p-1"
+            />
+        </div>
+        <MapContainer class="absolute inset-0 z-20" />
+        <LoadingPanel />
     </div>
-    <MapContainer class="absolute inset-0 z-20" />
-    <LoadingPanel />
-  </div>
 </template>
 
 <script setup lang="ts">
 import LoadingPanel from "./components/LoadingPanel.vue";
 import MapContainer from "./components/MapContainer.vue";
 </script>
-
